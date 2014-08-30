235f164e35ca5475e77a2dc49742a8a02f78aff4
tim lindner <tlindner@macmess.org>
2014-08-30 08:51:10 -0700

    Initial Commit

---
+++ DeXinet/TLAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  TLAppDelegate.m
+//  DeXinet
+//
+//  Created by Tim on 8/30/14.
+//  Copyright (c) 2014 Tim. All rights reserved.
+//
+
+#import "TLAppDelegate.h"
+
+@implementation TLAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
